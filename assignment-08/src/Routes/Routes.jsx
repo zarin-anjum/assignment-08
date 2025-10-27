17e5c3d566d1fe4db74e103e1fef32d402a1d342
zarin-anjum <zarinanjum0801@gmail.com>
2025-10-27 13:37:08 +0600

    completed navbar

--- assignment-08/src/Routes/Routes.jsx
+++ assignment-08/src/Routes/Routes.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { createBrowserRouter } from "react-router";
-import Navbar from '../components/Navbar/Navbar';
 import Root from '../pages/Root/Root';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import Home from '../pages/Home/Home';
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component: Root
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children:[
+        {
+            index: true,
+            path: "/",
+            Component: Home
+        }
+    ]
   },
 ]);
--- assignment-08/src/components/Navbar/Navbar.jsx
+++ assignment-08/src/components/Navbar/Navbar.jsx
@@ -1,78 +1,86 @@
 import React from "react";
+import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            </div>
-            <ul
-              tabIndex="-1"
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+    <div className="navbar bg-base-100 shadow-sm px-8 md:px-12 lg:px-20 py-4 lg:py-5">
+      <div className="navbar-start flex items-center gap-2">
+        <img
+          src="/assets/logo.png"
+          alt="HERO.IO logo"
+          className="w-5 md:w-8 lg:w-10 h-5 md:h-8 lg:h-10"
+        />
+        <a className="font-bold text-sm md:text-[16px] lg:text-lg bg-linear-to-r from-[#632EE3] to-[#9F62F2] bg-clip-text text-transparent">HERO.IO</a>
+      </div>
+
+      <div className="navbar-center hidden md:flex">
+        <ul className="flex gap-5 font-medium text-sm md:text-[16px] lg:text-lg">
+          <li>
+            <a className="hover:text-purple-600 hover:underline">Home</a>
+          </li>
+          <li>
+            <a className="hover:text-purple-600 hover:underline">Apps</a>
+          </li>
+          <li>
+            <a className="hover:text-purple-600 hover:underline">Installation</a>
+          </li>
+        </ul>
+      </div>
+
+      
+      <div className="navbar-end flex items-center gap-4">
+        <a
+          href="https://github.com/zarin-anjum"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hidden md:flex btn bg-linear-to-r from-[#632EE3] to-[#9F62F2]  hover:from-[#9F62F2] hover:to-[#632EE3] font-semibold text-white text-sm md:text-[16px] lg:text-lg rounded-sm"
+        >
+          <FaGithub className="text-lg" />
+          Contribute
+        </a>
+
+        <div className="dropdown dropdown-end md:hidden">
+          <div tabIndex={0} role="button" className="btn btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
-            </ul>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-10 mt-3 w-52 p-2 shadow font-normal text-sm md:text-[16px] lg:text-lg"
+          >
             <li>
-              <a>Item 1</a>
+              <a>Home</a>
             </li>
             <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
+              <a>Apps</a>
             </li>
             <li>
-              <a>Item 3</a>
+              <a>Installation</a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/zarin-anjum"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] font-semibold text-white text-sm md:text-[16px] lg:text-lg rounded-sm"
+              >
+                <FaGithub className="text-lg" />
+                Contribute
+              </a>
             </li>
           </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
         </div>
       </div>
     </div>
